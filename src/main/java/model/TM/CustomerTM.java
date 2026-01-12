1d9d28d04b23d504374380a17b3d003b826d3ac9
rash-ad <Rashad.mohd2004@gmail.com>
2026-01-12 21:59:41 +0530

    added a simple crud buttons without any error

--- src/main/java/model/TM/CustomerTM.java
+++ src/main/java/model/TM/CustomerTM.java
@@ -1,35 +1,32 @@
-package model.TM;
+package model.tm;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.Date;
-@Getter
-@Setter
-@AllArgsConstructor
+
+@Data
+@NoArgsConstructor
 @ToString
 public class CustomerTM {
     private String id;
     private String name;
-    private String address;
     private Date dob;
     private Double salary;
+    private String address;
     private String city;
     private String province;
     private String postalCode;
 
-
-    public CustomerTM(String id, String title, String name, String postalCode, String address, Date dob, Double salary, String city, String province) {
-        this.id=id;
-        this.name =title+":"+name;
-        this.address=address;
-        this.dob=dob;
-        this.salary=salary;
-        this.city=city+" : "+this.postalCode;
-        this.province=province;
-
-        
+    public CustomerTM(String id,String title, String name, Date dob, Double salary, String address, String city, String province, String postalCode) {
+        this.id = id;
+        this.name =title +":"+ name;
+        this.dob = dob;
+        this.salary = salary;
+        this.address = address;
+        this.city = city;
+        this.province = province;
+        this.postalCode = postalCode;
     }
 }
